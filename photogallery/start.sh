bb166508a42859f8e8327e63ec28c7a361a5d8e8
Teemu Paananen <teemu.paananen@gmail.com>
2017-06-13 18:39:41 +0300

    Cosmetic changes

--- photogallery/start.sh
+++ photogallery/start.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 python manage.py migrate
 python manage.py process_tasks &
-gunicorn \
+gunicorn photogallery.wsgi:application \
 --workers 6 \
 --bind 0.0.0.0:8000 \
 --reload \
 --access-logfile - \
 --access-logformat '%(r)s %(s)s %(b)s %(L)ss' \
-photogallery.wsgi:application
