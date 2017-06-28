276164e24d4335248f6365ea75998f8f866f236e
Teemu Paananen <teemu.paananen@gmail.com>
2017-06-28 07:25:13 +0300

    Added photo thumbnails to editing forms

--- photogallery/photogallery/settings.py
+++ photogallery/photogallery/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'photogallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':       'django.db.backends.postgresql_psycopg2',
+        'ENGINE':       'django.db.backends.postgresql',
         'NAME':         os.environ['DB_NAME'],
         'USER':         os.environ['DB_USER'],
         'PASSWORD':     os.environ['DB_PASS'],
