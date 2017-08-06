b1743c199d42bca4f6908ad7307551cf5ddc3c27
Teemu Paananen <teemu.paananen@gmail.com>
2017-08-06 14:34:27 +0300

    Customized Django admin header

--- photogallery/gallery/admin.py
+++ photogallery/gallery/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from django.utils.translation import ugettext_lazy as _
 
 from gallery.models import Album, Photo
+
+
+class GalleryAdminSite(AdminSite):
+    site_title = _('Gallery administration')
+    site_header = _('Gallery administration')
 
 
 class AlbumAdmin(admin.ModelAdmin):
@@ -56,5 +62,7 @@ class PhotoAdmin(admin.ModelAdmin):
     readonly_fields = ('file_hash',)
     list_filter = ('album', 'ready')
 
+
+admin.site = GalleryAdminSite()
 admin.site.register(Album, AlbumAdmin)
 admin.site.register(Photo, PhotoAdmin)
