5964ef7d8c05a8d7bab859da52f4547a70ebb863
Teemu Paananen <teemu.paananen@gmail.com>
2017-09-24 12:39:40 +0300

    Cosmetic changes to urls.py imports

--- photogallery/photogallery/urls.py
+++ photogallery/photogallery/urls.py
@@ -1,5 +1,7 @@
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.contrib import admin
 
 """photogallery URL Configuration
 
@@ -16,8 +18,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
