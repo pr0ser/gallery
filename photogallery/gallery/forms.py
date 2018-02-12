358dc03bdcda149c972163996acc40f2fbdcfc63
Teemu Paananen <teemu.paananen@gmail.com>
2018-02-12 21:29:36 +0200

    Remove image field from photo edit form

--- photogallery/gallery/forms.py
+++ photogallery/gallery/forms.py
@@ -59,7 +59,7 @@ class NewPhotoForm(ModelForm):
 class EditPhotoForm(ModelForm):
     class Meta:
         model = Photo
-        fields = ['title', 'description', 'image']
+        fields = ['title', 'description']
         widgets = {
             'album': forms.Select(attrs={'id': 'album'}),
         }
