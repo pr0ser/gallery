78de8d3546c1466179592f1ff10c4039532f590a
Teemu Paananen <teemu.paananen@gmail.com>
2018-02-12 22:41:51 +0200

    Launch more task processes

--- photogallery/start.sh
+++ photogallery/start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 python manage.py migrate
 nice -n 19 python manage.py process_tasks &
+nice -n 19 python manage.py process_tasks &
+nice -n 19 python manage.py process_tasks &
+nice -n 19 python manage.py process_tasks &
 gunicorn photogallery.wsgi:application \
 --workers 4 \
 --bind 0.0.0.0:8000 \
