082dfe64f455b2077916e6b257cea78ae3e9486d
Teemu Paananen <teemu.paananen@gmail.com>
2018-04-07 22:20:30 +0300

    Doubled gunicorn worker count

--- photogallery/start-prod.sh
+++ photogallery/start-prod.sh
@@ -2,7 +2,7 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 gunicorn photogallery.wsgi:application \
---workers 3 \
+--workers 6 \
 --bind unix:/run/gallery/gallery.socket \
 --timeout 300 \
 --log-level=info \
